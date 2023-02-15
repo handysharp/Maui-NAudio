721c4093edc8a814a442e0672072db869a132c24
handysharp <102320252+handysharp@users.noreply.github.com>
2023-02-14 19:19:03 -0500

    Add project files.

---
+++ Maui-NAudio/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Maui_NAudio.ViewModel;
+using Microsoft.Maui.Controls;
+using NAudio.Midi;
+
+
+namespace Maui_NAudio;
+/// <summary>
+/// Program is a .NetMAUI version of the console app at the url below:
+/// http://truelogic.org/wordpress/2021/01/28/using-midi-with-naudio/
+/// /// I have input the console version from the site above and it works. When a "midi in" message occurs from a keyboard, it goes to midiIn_MessageReceived(). But, with this version it does not go to that method and nothing prints out. I am not sure if my collectionview code is correct, but it is hard to tell without the midi info passing to it.
+/// </summary>
+
+public partial class MainPage : ContentPage
+{
+	public MainPage(MainViewModel vm)
+	{
+		BindingContext = vm;
+		MainViewModel.ListDevices();
+		InitializeComponent();
+		MainViewModel.Start();
+	}
+}
+
---
+++ Maui-NAudio/ViewModel/MainViewModel.cs
@@ -0,0 +1,76 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Collections;
+using CommunityToolkit.Mvvm.Input;
+using NAudio.Midi;
+using System.Collections.ObjectModel;
+
+namespace Maui_NAudio.ViewModel
+{
+	public partial class MainViewModel : ObservableObject
+	{
+		static int mInDeviceIndex = -1;
+		static int mOutDeviceIndex = -1;    // is this used?
+		static MidiIn mMidiIn = null;
+		static Boolean mReady = false;
+
+		[ObservableProperty]
+		ObservableCollection<string> items;
+
+		public MainViewModel()
+		{
+			Items = new ObservableCollection<string>();
+		}
+
+		[ObservableProperty]
+		static string deviceIn;
+		[ObservableProperty]
+		static string deviceOut;
+		[ObservableProperty]
+		static string outputLine;
+		[ObservableProperty]
+		static string errorLine;
+
+		static public void Start()
+		{
+			if(mReady)
+			{
+				HandleMidiMessages();
+				mMidiIn.Stop();
+				mMidiIn.Dispose();	
+			}
+		}
+
+		public static void ListDevices()
+		{
+			for (int device = 0; device < MidiIn.NumberOfDevices; device++)
+			{
+				mReady = true; //  some midi in device exists
+				deviceIn += $"{MidiIn.DeviceInfo(device).ProductName} \n";
+			}
+			mInDeviceIndex = 0;
+			for (int device = 0; device < MidiOut.NumberOfDevices; device++)
+			{
+				deviceOut += $"{MidiOut.DeviceInfo(device).ProductName} \n";
+			}
+			mOutDeviceIndex = 1;
+		}
+
+		public static void HandleMidiMessages()
+		{
+			mMidiIn = new MidiIn(mInDeviceIndex);
+			mMidiIn.MessageReceived += midiIn_MessageReceived;
+			mMidiIn.ErrorReceived += midiIn_ErrorReceived;
+			mMidiIn.Start();
+		}
+
+		public static void midiIn_ErrorReceived(object sender, MidiInMessageEventArgs e)
+		{
+			errorLine = String.Format("Time {0} Message 0x{1:X8} Event {2}", e.Timestamp, e.RawMessage, e.MidiEvent);
+		}
+
+		public static void midiIn_MessageReceived(object sender, MidiInMessageEventArgs e)
+		{
+			outputLine = String.Format("Time {0} Message 0x{1:X8} Event {2}", e.Timestamp, e.RawMessage, e.MidiEvent);
+		}
+	}
+}
